a927c0b60fa7415df3663504dd921aecff9daa4a
Valeriy <dobrotskiyvd@gmail.com>
2023-03-12 21:49:16 +0200

    Попытка настроить волны.

--- Assets/Scripts/TestScripts/TrajectoryMaker.cs
+++ Assets/Scripts/TestScripts/TrajectoryMaker.cs
@@ -12,15 +12,15 @@ public class TrajectoryMaker : MonoBehaviour
 
     public void ShowTrajectory(Vector3 origin, Vector3 velocity)
     {
-        if(_trajectoryLine.enabled == false)
+        if (_trajectoryLine.enabled == false)
             _trajectoryLine.enabled = true;
         List<Vector3> points = new List<Vector3>();
         int i = 0;
         float time = i * 0.1f;
         points.Add(origin + velocity * time + Physics.gravity * (time * time / 2f));
         i++;
-        
-        while (points[points.Count - 1].y > 0) 
+
+        while (points[points.Count - 1].y > transform.parent.parent.position.y - 5f)
         {
             time = i * 0.1f;
             points.Add(origin + velocity * time + Physics.gravity * (time * time / 2f));
